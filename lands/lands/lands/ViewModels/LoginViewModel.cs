58cc2f12058d588dbf0f8fa1ad937f95a6d4b396
Potter <mi_servicio@hotmail.com>
2020-01-25 12:55:27 -0600

    login modificado

--- lands/lands/lands/ViewModels/LoginViewModel.cs
+++ lands/lands/lands/ViewModels/LoginViewModel.cs
@@ -5,40 +5,87 @@ using System.Text;
 
 namespace lands.ViewModels
 {
-
+    using GalaSoft.MvvmLight.Command;
+    using System.ComponentModel;
     using System.Windows.Input;
-    class LoginViewModel
+    using Xamarin.Forms;
+
+    class LoginViewModel : BaseViewModel
     {
+
+        
+
+        #region Attributes
+        private string email;
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        private bool isRemmembered;
+        #endregion
+
         #region Propierties
         public String Email {
-            get; 
-            set;
-        
+            get { return this.email; }
+            set { SetValue(ref this.email, value); }
+
         }
 
         public String Password {
-            get; 
-            set; 
+
+            get { return password; }
+            set { SetValue(ref password, value); }
+            
+            
+            /*get 
+            {
+                return this.password;
+            } 
+
+            set
+            {
+                if(this.password != value)
+                {
+                    this.password = value;
+
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(this.password)));
+                }
+            }*/
         }
 
         public bool IsRunning
         {
-            get; 
-            set; 
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
         }
 
 
-        public bool IsRemmember
-        { 
-            get; 
-            set; 
+        public bool IsRemmembered
+        {
+            get { return this.isRemmembered; }
+            set { SetValue(ref this.isRemmembered, value); }
         }
+
+
+        public bool IsEnabled 
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
+
         #endregion
+
+
+
+
+
 
         #region Contructors
         public LoginViewModel()
         {
-            this.IsRemmember = true;
+            this.isRemmembered = true;
+            this.isEnabled = true;
         }
         
         #endregion
@@ -46,9 +93,73 @@ namespace lands.ViewModels
 
         #region Commands
         public ICommand LoginCommand
-        { 
-            get; 
-            set; 
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+            
+        }
+
+        
+
+        private async void Login()
+        {
+
+
+            if(string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an Password",
+                    "Accept");
+            }
+            
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an Email ",
+                    "Accept");
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+
+            if (this.Email != "mi_servicio@hotmail.com"||this.Password != "1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+
+                await Application.Current.MainPage.DisplayAlert(
+
+                    "Error",
+                    "Email or password incorret.",
+                    "Accept"
+
+
+                    );
+
+
+                    this.Password = string.Empty;
+                
+
+               
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            await Application.Current.MainPage.DisplayAlert(
+                   "ok",
+                   "Fuck yea!!!",
+                   "Accept");
+
+
+
+
         }
 
         #endregion
